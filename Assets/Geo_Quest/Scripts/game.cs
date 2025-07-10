25456ddf6810844b2a579ed0082053110ba24372
orio-na <ashkenaziorion@gmail.com>
2025-07-10 12:18:14 -0400

    Classwork#3
    Created effectors for marble game, coded movement for cubes. also coded part of death spike mechanism. transformation.

---
+++ Assets/Geo_Quest/Scripts/game.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xSpeed * speed, rb.velocity.y);
+    }
+      /*
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+           transform.position += new Vector3(0,1,0);
+        }
+       if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0,-1,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(.008f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-.008f, 0, 0);
+        }*/
+    
+}
